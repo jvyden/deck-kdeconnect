b4bdac0333874511d0cca70882123e14f56fd914
jvyden <jvyden@jvyden.xyz>
2023-02-28 19:45:24 -0500

    fix stopping

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import subprocess
 
 
 class Plugin:
+    bus = "unix:path=/run/user/1000/bus"
+    env = {
+            'DBUS_SESSION_BUS_ADDRESS': bus
+    }
+
     async def start_service(self):
         args = ["/usr/bin/systemd-run",
             "--user",
             "--scope",
             "--setenv=DISPLAY=:0",
-            "--setenv=DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus",
+            "--setenv=DBUS_SESSION_BUS_ADDRESS=" + self.bus,
             "--uid", "1000",
             "--unit", "deck-kdeconnectd",
             "/usr/lib/kdeconnectd"
         ]
 
-        env = {
-            'DBUS_SESSION_BUS_ADDRESS': 'unix:path=/var/run/dbus/system_bus_socket'
-        }
-
-        subprocess.Popen(args, stdout=None, stderr=None, stdin=subprocess.DEVNULL, start_new_session=True, env=env)
+        subprocess.Popen(args, stdout=None, stderr=None, stdin=subprocess.DEVNULL, start_new_session=True, env=self.env)
         return 0
 
     async def stop_service(self):
-        subprocess.run(["/usr/bin/killall", 'kdeconnectd'])
+        subprocess.run(["/usr/bin/systemctl", '--user', 'stop', 'deck-kdeconnectd.scope'], check=True, env=self.env)
         return 0
